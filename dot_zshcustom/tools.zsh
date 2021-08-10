4e39ee7434ecc3d906d846696eab38432878c50c
roconnorr <roryoconnor@outlook.co.nz>
2021-08-11 11:55:36 +1200

    gh aliases

--- dot_zshcustom/tools.zsh
+++ dot_zshcustom/tools.zsh
@@ -61,6 +61,45 @@ alias fzfrb="echo '' | fzf --print-query --preview 'ruby -e {q}'"
 
 #############################################
 #
+# github
+#
+#############################################
+function pr-checkout() {
+  local jq_template pr_number
+
+  jq_template='"'\
+'#\(.number) - \(.title)'\
+'\t'\
+'Author: \(.user.login)\n'\
+'Created: \(.created_at)\n'\
+'Updated: \(.updated_at)\n\n'\
+'\(.body)'\
+'"'
+
+  pr_number=$(
+    gh api 'repos/:owner/:repo/pulls' |
+    jq ".[] | $jq_template" |
+    sed -e 's/"\(.*\)"/\1/' -e 's/\\t/\t/' |
+    fzf \
+      --with-nth=1 \
+      --delimiter='\t' \
+      --preview='echo -e {2}' \
+      --preview-window=top:wrap |
+    sed 's/^#\([0-9]\+\).*/\1/'
+  )
+
+  if [ -n "$pr_number" ]; then
+    gh pr checkout "$pr_number"
+  fi
+}
+
+function pr-list() {
+  gh api 'repos/:owner/:repo/pulls' |
+    jq --raw-output '.[] | "#\(.number) \(.title)"'
+}
+
+#############################################
+#
 # general
 #
 #############################################
