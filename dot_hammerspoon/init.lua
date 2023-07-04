27cf327f2bf515899a078edd20621be696d11491
Rory O'Connor <rory.oconnor@dovetailstudios.com>
2023-07-04 18:12:54 +1200

    comment hammerspoon window management

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -20,44 +20,44 @@ end)
 
 detectLogiMouseExtraButtons:start()
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
+--   f.x = max.x
+--   f.y = max.y
+--   f.w = max.w / 2
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
+--   f.x = max.x + (max.w / 2)
+--   f.y = max.y
+--   f.w = max.w / 2
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
-end)
+--   f.x = max.x
+--   f.y = max.y
+--   f.w = max.w
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
 
 function moveWindowToDisplay(direction)
   return function()
